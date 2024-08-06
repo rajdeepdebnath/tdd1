3d2ec7bb8acfa54042d0e34efb8fc9ff5689beaa
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2024-08-06 14:47:50 +0530

    returns 1 for single array argument i.e. ['1']

--- index.test.js
+++ index.test.js
@@ -11,3 +11,7 @@ test("returns 0 for empty string", () => {
 test("returns 0 for null argument", () => {
   expect(add()).toBe(0);
 });
+
+test("returns 1 for single array argument i.e. ['1']", () => {
+  expect(add("1")).toBe(1);
+});
