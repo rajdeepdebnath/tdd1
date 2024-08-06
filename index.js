de0c3d470fc89253ba95ec97ee9b844106dff4f1
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2024-08-06 14:43:10 +0530

    returns 0 for empty string

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 export function add(numbersStr) {
+  if (!numbersStr || numbersStr.trim() === "") return 0;
+
   const numbers = numbersStr.split(",");
+
   let sum = 0;
   for (let num of numbers) {
     sum += parseInt(num);
--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ import { add } from "./index.js";
 test("adds 1 + 2 to equal 3", () => {
   expect(add("1, 2")).toBe(3);
 });
+
+test("returns 0 for empty string", () => {
+  expect(add("")).toBe(0);
+});
