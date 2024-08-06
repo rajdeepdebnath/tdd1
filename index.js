8d25881216adc9d3d15b48602d5b8108f2fc8fc8
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2024-08-06 15:04:33 +0530

    adds with new lines between numbers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 export function add(numbersStr) {
   if (!numbersStr || numbersStr.trim() === "") return 0;
 
-  const numbers = numbersStr.split(",");
-  console.log(numbers);
+  const numbers = numbersStr.replace(/\n/g, ",").split(",");
+
   let sum = 0;
   for (let num of numbers) {
     if (/\d/.test(num)) {
--- index.test.js
+++ index.test.js
@@ -12,10 +12,18 @@ test("returns 0 for null argument", () => {
   expect(add()).toBe(0);
 });
 
-test("returns 1 for single array argument i.e. '1'", () => {
+test("returns 1 for single argument i.e. '1'", () => {
   expect(add("1")).toBe(1);
 });
 
-test("returns 1 for array argument with empty string i.e. '1, '", () => {
+test("returns 1 for argument with empty string i.e. '1, '", () => {
   expect(add("1, ")).toBe(1);
+});
+
+test("returns 1 for argument with invalid string i.e. '1, null'", () => {
+  expect(add("1, null")).toBe(1);
+});
+
+test("adds with new lines between numbers", () => {
+  expect(add("1\n2,3")).toBe(6);
 });
